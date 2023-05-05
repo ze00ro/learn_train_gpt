9d04393a76b7c2cd1c95e509e8dab118a3b3f8ba
ze00ro <721908811@qq.com>
2023-05-05 13:53:59 +0800

    test

--- web/basic_gradio.py
+++ web/basic_gradio.py
@@ -1,5 +1,5 @@
 from transformers import GPT2LMHeadModel, BertTokenizer
-import gradio
+import gradio as gr
 
 # model = GPT2LMHeadModel.from_pretrained(name, pad_token_id=tokenizer.eos_token_id)
 
@@ -10,20 +10,17 @@ model = GPT2LMHeadModel.from_pretrained(model_id)
 
 
 def predict(inp):
-    input_ids = tokenizer.encode(inp, return_tensors='tf')
+    input_ids = tokenizer.encode(inp, return_tensors='pt')
     beam_output = model.generate(input_ids, max_length=100, num_beams=5, no_repeat_ngram_size=2, early_stopping=True)
     output = tokenizer.decode(beam_output[0], skip_special_tokens=True, clean_up_tokenization_spaces=True)
     return ".".join(output.split(".")[:-1]) + "."
 
 
-INPUTS = gradio.inputs.Textbox()
-OUTPUTS = gradio.outputs.Textbox()
-INTERFACE = gradio.Interface(fn=predict,
-                             inputs=INPUTS,
-                             outputs=OUTPUTS,
-                             title="GPT-2 训练演示",
-                             description="使用 GPT-2 模型进行文本生成",
-                             thumbnail="",
-                             capture_session=False)
+demo = gr.Interface(fn=predict,
+                    inputs="text",
+                    outputs="text",
+                    title="GPT-2 训练演示",
+                    description="使用 GPT-2 模型进行文本生成",
+                    )
 
-INTERFACE.launch(inbrowser=True)
+demo.queue(concurrency_count=3).launch(server_name='0.0.0.0', share=True, inbrowser=False)
