0ed27315ee89fdfd8c9a0285cf68c6f593cd00d3
ze00ro <721908811@qq.com>
2023-05-05 14:08:59 +0800

    test

--- ui_basic_gradio.py
+++ ui_basic_gradio.py
@@ -14,17 +14,23 @@ tokenizer = BertTokenizer.from_pretrained(model_id)
 model = GPT2LMHeadModel.from_pretrained(model_id)
 
 
-def predict(input):
+def predict(input1):
     text_generator = TextGenerationPipeline(model=model, tokenizer=tokenizer)
-    res = text_generator(input, max_length=100, do_sample=True)
+    res = text_generator(input1, max_length=100, do_sample=True)
     return res
 
+
+examples = [
+    ["太师椅"],
+    ["好的空调"],
+]
 
 demo = gr.Interface(fn=predict,
                     inputs="text",
                     outputs="text",
                     title="GPT-2 训练演示",
                     description="使用 GPT-2 模型进行文本生成",
+                    examples=examples,
                     )
 
-demo.queue(concurrency_count=3).launch(server_name='0.0.0.0', share=True, inbrowser=False)
+demo.queue(concurrency_count=3).launch(server_name='0.0.0.0', share=False, inbrowser=False)
