764abb8433dee04f692988c68b322b623caf72da
ze00ro <721908811@qq.com>
2023-05-25 11:35:18 +0800

    test

--- ui_basic_gradio.py
+++ ui_basic_gradio.py
@@ -40,7 +40,7 @@ examples = [
 ]
 
 demo = gr.Interface(fn=predict_by_pipe,
-                    inputs=["text", gr.Slider(0.0, 1.0, step=0.1, value=0.2)],
+                    inputs=["text", gr.Slider(0.1, 1.0, step=0.1, value=0.1)],
                     outputs="text",
                     title="GPT-2 训练演示",
                     description="使用 GPT-2 模型进行文本生成 4batch, 6epoch, 1e-3",
